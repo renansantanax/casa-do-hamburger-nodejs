0ed1f99e49f7577e647fcb44bac2dadd60f19e41
renansantanax <renan.2r@live.com>
2026-05-18 19:30:04 -0300

    feat: Rota de login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,15 +2,54 @@ import { Link } from "react-router";
 import Input from "../components/Input";
 import { useState } from "react";
 import Button from "../components/Button";
+import { useNavigate } from "react-router";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(email);
-    console.log(password);
+    try {
+      if (!email || !password) {
+        setError("Email e senha são obrigatórios");
+        return;
+      }
+
+      const response: Response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.status === 400) {
+        setError("Email e senha são obrigatórios");
+        return;
+      }
+
+      if (response.status === 401) {
+        setError("Email ou senha incorretos");
+        return;
+      }
+
+      if (response.status === 404) {
+        setError("Usuário não encontrado");
+        return;
+      }
+
+      if (response.status === 200) {
+        setError("");
+        const json = await response.json();
+        navigate("/");
+        console.log(json);
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      return;
+    }
   };
 
   return (
@@ -18,24 +57,29 @@ const Login = () => {
       className="flex h-screen items-center justify-center bg-[#161410]"
       onSubmit={handleSubmit}
     >
-      <div className="flex flex-col items-center justify-center gap-2">
+      <div className="flex flex-col justify-center gap-2">
         <Link to="/">
-          <img className="mb-4" src="./logo.png" alt="" />
+          <img className="mx-auto mb-4" src="./logo.png" alt="" />
         </Link>
-        <Input
-          placeholder="Email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <Input
-          placeholder="Senha"
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <Button title="Fazer login" />
+        <div className="mb-3 flex flex-col gap-2">
+          {" "}
+          <Input
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+          />
+          <Input
+            placeholder="Senha"
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <p className="text-left text-sm text-red-500">{error}</p>
+        </div>
+        <Button title="Fazer login" type="submit" />
         <Link to="/register" className="w-full">
           <Button title="Não tem uma conta" variant="outline" />
         </Link>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -7,12 +7,60 @@ const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirm, setConfirmPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [cep, setCep] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log({ name, email, password, confirm, cep });
+    try {
+      if (!name || !email || !password || !cep) {
+        alert("Todos os campos são obrigatórios");
+        setError("Todos os campos são obrigatórios");
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        setError("As senhas não coincidem");
+        return;
+      }
+
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password, cep }),
+      });
+
+      switch (response.status) {
+        case 409:
+          setError("Email já cadastrado");
+          break;
+        case 400:
+          setError("Todos os campos são obrigatórios");
+          break;
+        case 500:
+          setError("Erro interno do servidor");
+          break;
+        case 201:
+          setName("");
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
+          setCep("");
+          setError("");
+          break;
+        default:
+          setError("");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Erro ao registrar:", error);
+      return;
+    }
   };
 
   return (
@@ -20,9 +68,9 @@ const Register = () => {
       className="flex h-screen items-center justify-center bg-[#161410]"
       onSubmit={handleSubmit}
     >
-      <div className="flex flex-col items-center justify-center gap-2">
+      <div className="flex flex-col justify-center gap-2">
         <Link to="/">
-          <img className="mb-4" src="./logo.png" alt="" />
+          <img className="mx-auto mb-4" src="./logo.png" alt="" />
         </Link>
         <Input
           placeholder="Nome"
@@ -30,6 +78,7 @@ const Register = () => {
           onChange={(e) => {
             setName(e.target.value);
           }}
+          value={name}
         />
         <Input
           placeholder="Email"
@@ -37,6 +86,7 @@ const Register = () => {
           onChange={(e) => {
             setEmail(e.target.value);
           }}
+          value={email}
         />
         <Input
           placeholder="Password"
@@ -44,21 +94,27 @@ const Register = () => {
           onChange={(e) => {
             setPassword(e.target.value);
           }}
+          value={password}
         />
         <Input
           placeholder="Confirme sua senha"
           type="password"
           onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
         />
         <Input
           placeholder="CEP"
           type="text"
           onChange={(e) => setCep(e.target.value)}
+          value={cep}
         />
-        <Button title="Criar conta" />
-        <Link to="/login" className="w-full">
-          <Button title="Já tenho uma conta" variant="outline" />
-        </Link>
+        <p className="text-left text-red-500">{error}</p>
+        <div className="mt-3 flex w-full flex-col gap-2">
+          <Button title="Criar conta" type="submit" />
+          <Link to="/login" className="w-full">
+            <Button title="Já tenho uma conta" variant="outline" />
+          </Link>
+        </div>
       </div>
     </form>
   );
